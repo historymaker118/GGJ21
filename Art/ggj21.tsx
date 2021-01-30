da5c8872e6c5d6a1cc714eed85a3f2be0d569d80
Rebecca Brannum <historymaker118@googlemail.com>
2021-01-30 11:34:12 +0000

    Updated tilesheet

---
+++ Art/ggj21.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="ggj21" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="Tilesheet.png" trans="000000" width="128" height="128"/>
+</tileset>
